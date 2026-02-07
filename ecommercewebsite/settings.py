149743c9e29bbdff6d3ac4d7f741cc62edd86a2f
unknown <krishna>
2026-02-07 10:57:46 -0800

    fix static files

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = [BASE_DIR/"static"]
-STATIC_ROOT = BASE_DIR/"staticfiles"
+STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS =[STATIC_DIR]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
