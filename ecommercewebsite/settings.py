fd66b5d9f4db8a9061f842808d6d46a22bcf00b2
unknown <krishna>
2026-02-07 04:08:37 -0800

    fix db config for render

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -75,24 +75,31 @@ WSGI_APPLICATION = 'ecommercewebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecommerceproject',
+# 		'USER': 'postgres',
+#         'PASSWORD': 'Krish@2005',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecommerceproject',
-		'USER': 'postgres',
-        'PASSWORD': 'Krish@2005',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+if os.environ.get("DATABASE_URL"):
+    DATABASES['default']=dj_database_url.config(conn_max_age=600,ssl_required=True)
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
