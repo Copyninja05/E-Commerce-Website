210e355848f59ce9e0bcc54e4a69b08a932a47a0
unknown <krishna>
2026-02-09 00:03:11 -0800

    database success

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     'default':dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_required=True
+        ssl_require=True
     )
 }
 # Password validation
