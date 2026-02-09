6f533dfc4f6833fff4d5c3bb6b1d97bb4bf54ba6
unknown <krishna>
2026-02-08 23:56:27 -0800

    database success

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -91,16 +91,13 @@ WSGI_APPLICATION = 'ecommercewebsite.wsgi.application'
 
 import os
 import dj_database_url
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_required=True
+    )
 }
-
-if os.environ.get("DATABASE_URL"):
-    DATABASES['default']=dj_database_url.config(conn_max_age=600,ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
