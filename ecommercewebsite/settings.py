c01dd67117bd8c2e460fa21042007e2e3f00d601
unknown <krishna>
2026-02-09 01:25:22 -0800

    database success

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -93,9 +93,9 @@ import os
 import dj_database_url
 DATABASES = {
     'default':dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+        default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
         conn_max_age=600,
-        ssl_require=True
+       
     )
 }
 # Password validation
