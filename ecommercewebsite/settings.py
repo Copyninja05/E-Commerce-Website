f037e06e9c4ddd90b1cb6743f3dcad734d5d982a
unknown <krishna>
2026-02-08 22:55:57 -0800

    all right

--- ecommercewebsite/settings.py
+++ ecommercewebsite/settings.py
@@ -134,15 +134,23 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+from pathlib import Path
+import os
+
+BASE_DIR = Path(_file_).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR / 'staticfiles'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICEFILES_STORAGE='whitenoise.storage.compressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
