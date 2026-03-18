c9bf7116cc13d88ba9963973cf1cfbc35921282e
unknown <krishna>
2026-03-17 20:40:52 -0700

    done

--- adm/views.py
+++ adm/views.py
@@ -5,6 +5,22 @@ from .models import *
 
 
 
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='srinivasan',
+            email='admin@gmail.com',
+            password='srin@1968'
+        )
+        return HttpResponse("Admin created successfully!")
+    else:
+        return HttpResponse("Admin already exists!")
+
+
+
 
 
 
